fb5a88e3cf871ebe1e7ee3219ba406e2fffd255a
beardfaceguy <zombiebot@gmail.com>
2024-08-12 13:03:27 -0700

    finished pda app

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,1 +1,54 @@
+//When importing css files, need to point directly to the css file, including the extension
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+//when importing image files, need to include the extension
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Personal Digital Assitants</p>
+          </div>
+        </section>
+      </div>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Amazon assistant"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortanaBlue"
+                image={CortanaImage}
+                description="Cortana is a microsoft assistant"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@heySiri"
+                image={SiriImage}
+                description="Siri's being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,5 +1,28 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+// The below change to the argument is an example of
+//argument destructuring, where create new variables
+//and assign them to the values of the same name in
+//the argument object being passed in
+
+function ProfileCard({ title, handle, image, description }) {
+  console.log(
+    `ProfileCard props \n   title: ${title}\n   handle: ${handle}\n image:${image}`
+  );
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="Personal Digital Assistant logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
