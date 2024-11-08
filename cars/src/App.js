7e554973e67e525b5624f2b87645375e4ca83a0f
beardfaceguy <zombiebot@gmail.com>
2024-11-07 18:12:57 -0800

    cars

--- cars/src/App.js
+++ cars/src/App.js
@@ -1,3 +1,15 @@
+/*
+Redux store design
+  1. Identify what state exists in the app
+  2. Identify how that state changes over time
+  3. Group together common pieces of state
+  4. Create a slice for each group
+
+
+
+
+*/
+
 import CarForm from './components/CarForm';
 import CarList from './components/CarList';
 import CarSearch from './components/CarSearch';
--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,5 +1,40 @@
+/*
+Accessing State
+  1. Find the component that needs to access some state
+  2. Import the 'useSelector' hook from 'react-redux'
+  3. Call the hook, passing in a selector function
+  4. Use the state! Anytime state changes, the component will automatically rerender
+*/
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store';
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const nameStr = useSelector((state) => {
+    return state.form.name;
+  });
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={nameStr}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
