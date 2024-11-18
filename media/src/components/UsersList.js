e0ff8a63ca92719a2ea6d1e849d082335b46e85d
beardfaceguy <zombiebot@gmail.com>
2024-11-17 20:09:54 -0800

    media project work

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,5 +1,58 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Skeleton from './Skeleton';
+import Button from './Button';
+import { useThunk } from '../hooks/use-thunk';
+
 function UsersList() {
-  return 'Users List';
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+  // the warning below about the missing depenency on the second argument is erronious from eslint
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  let content;
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+
+        {creatingUserError && 'Error creating user...'}
+      </div>
+      {content}
+    </div>
+  );
 }
 
 export default UsersList;
--- media/src/store/slices/usersSlice.js
+++ media/src/store/slices/usersSlice.js
@@ -1,11 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
+import { addUser } from '../thunks/addUser';
+
+/*
+Creating an Async Thunk
+
+1.  Create a new file for your thunk.  Name it after the purpose of the request
+2.  Create the thunk.  Give it a base type that describes the purpose of the request
+3.  In the thunk, make the request, return the data that you want to use in the reducer
+4.  In the slice, add extraReducers, watching for the action types made by the thunk
+
+*/
 
 const usersSlice = createSlice({
   name: 'users',
   initialState: {
     data: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // Update state object however appropriate
+      // to show the user we are loading data
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+    builder.addCase(addUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data.push(action.payload);
+    });
+    builder.addCase(addUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
