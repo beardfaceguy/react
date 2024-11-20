f0ab271ada6f0fe36c32c9fc5b376b7066eb6dc7
beardfaceguy <zombiebot@gmail.com>
2024-11-19 17:45:36 -0800

    middle of photo album work

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -17,7 +17,7 @@
 */
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { faker } from '@faker-js/faker';
 const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({
@@ -25,6 +25,18 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      addAlbum: builder.mutation({
+        query: (user) => {
+          return {
+            url: '/albums',
+            method: 'POST',
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
       fetchAlbums: builder.query({
         query: (user) => {
           return {
@@ -40,7 +52,7 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
 
 /*
